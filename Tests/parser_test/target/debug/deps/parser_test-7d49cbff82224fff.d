06ba88761be751e4a605425a0567dee4216c93f3
Jam0230 <bmagraham227@gmail.com>
2024-03-21 09:41:59 +0000

    New Test Projects

--- Tests/parser_test/target/debug/deps/parser_test-7d49cbff82224fff.d
+++
@@ -1,5 +0,0 @@
-/home/jam/Documents/School/ComputerScience/NEA/Tests/parser_test/target/debug/deps/libparser_test-7d49cbff82224fff.rmeta: src/main.rs
-
-/home/jam/Documents/School/ComputerScience/NEA/Tests/parser_test/target/debug/deps/parser_test-7d49cbff82224fff.d: src/main.rs
-
-src/main.rs:
--- Tests/scanner_test/target/debug/deps/scanner_test-292a8af628746876.d
+++
@@ -1,5 +0,0 @@
-/home/jam/Documents/School/ComputerScience/NEA/Tests/scanner_test/target/debug/deps/libscanner_test-292a8af628746876.rmeta: src/main.rs
-
-/home/jam/Documents/School/ComputerScience/NEA/Tests/scanner_test/target/debug/deps/scanner_test-292a8af628746876.d: src/main.rs
-
-src/main.rs:
